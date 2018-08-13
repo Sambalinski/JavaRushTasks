68a712ef1c18c67182b89ca4d3bc5cee8122b07b
Johnny Sambalinski <sambalinski@mail.ru>
2018-08-13 09:17:06 +0300

    Перешёл на 25 уровень

--- 4.JavaCollections/src/com/javarush/task/task33/task3303/Solution.java
+++ 4.JavaCollections/src/com/javarush/task/task33/task3303/Solution.java
@@ -1,16 +1,25 @@
 package com.javarush.task.task33.task3303;
 
-import java.io.FileReader;
-import java.io.IOException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.*;
 
 /* 
 Десериализация JSON объекта
 */
 public class Solution {
     public static <T> T convertFromJsonToNormal(String fileName, Class<T> clazz) throws IOException {
-        try(FileReader fileReader = new FileReader(fileName)){
+        ObjectMapper mapper = new ObjectMapper();
 
-        }catch (IOException e){}
+        File file = new File(fileName);
+
+        T t = mapper.readValue(file, clazz);
+
+            //Альтернатива
+//        FileReader reader = new FileReader(new File(fileName));
+//        T t = mapper.readValue(reader, clazz);
+
+        return t;
     }
 
     public static void main(String[] args) {
---
+++ 4.JavaCollections/src/com/javarush/task/task33/task3304/Solution.java
@@ -0,0 +1,56 @@
+package com.javarush.task.task33.task3304;
+
+import com.fasterxml.jackson.annotation.JsonSubTypes;
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.IOException;
+import java.io.StringReader;
+import java.io.StringWriter;
+
+/* 
+Конвертация из одного класса в другой используя JSON
+*/
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        Second s = (Second) convertOneToAnother(new First(), Second.class);
+        First f = (First) convertOneToAnother(new Second(), First.class);
+
+//        System.out.println(s.getClass());
+//        System.out.println(f.getClass());
+    }
+
+    public static Object convertOneToAnother(Object one, Class resultClassObject) throws IOException {
+        StringWriter writer = new StringWriter();
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.writeValue(writer, one);
+
+        String JSON = writer.toString();
+
+        System.out.println(JSON);
+
+        String JSONReplacement = JSON.replaceFirst(one.getClass().getSimpleName().toLowerCase(), resultClassObject.getSimpleName().toLowerCase());
+
+//        System.out.println(JSONReplacement);
+
+        StringReader reader = new StringReader(JSONReplacement);
+        mapper = new ObjectMapper();
+        Object o = mapper.readValue(reader, resultClassObject);
+
+        return o;
+    }
+
+    @JsonTypeInfo(use = JsonTypeInfo.Id.NAME,  property="className")
+    @JsonSubTypes(@JsonSubTypes.Type(value=First.class,  name="first"))
+    public static class First {
+        public int i;
+        public String name;
+    }
+
+    @JsonTypeInfo(use = JsonTypeInfo.Id.NAME,  property="className")
+    @JsonSubTypes(@JsonSubTypes.Type(value=Second.class, name="second"))
+    public static class Second {
+        public int i;
+        public String name;
+    }
+}
